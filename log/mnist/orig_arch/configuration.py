c91bd573ccfc6719215d80efcc3e40dbc309e5a0
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-11-29 10:56:14 +0000

    New run mnist

--- log/mnist/orig_arch/configuration.py
+++ log/mnist/orig_arch/configuration.py
@@ -1,6 +1,6 @@
-# Training started at: 2018-11-29 10:42:51.954800
-# Training ended at: 2018-11-29 10:42:51.954800
-# Training duration: 0h 2m 14.04s
+# Training started at: 2018-11-29 10:50:19.680499
+# Training ended at: 2018-11-29 10:50:19.680499
+# Training duration: 0h 4m 54.73s
 from pathlib import Path
 import datetime
 class Configuration(object):
