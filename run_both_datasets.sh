4bbed7daaa6290f9d8bb3772f405cf8bccd2b21a
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-12-21 22:32:08 +0000

    New runs with 500 epochs

--- run_both_datasets.sh
+++ run_both_datasets.sh
@@ -2,6 +2,7 @@
 
 exp_name=$1
 epochs=$2
+epochs2=$3
 
 python3 models.py -d prosivic -e $epochs -x $exp_name &&
-python3 models.py -d dreyeve -e $epochs -x $exp_name
+python3 models.py -d dreyeve -e $epochs2 -x $exp_name
